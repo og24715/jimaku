5e7d1f38d26b4f62c0d46a04d1a6759fee445760
hirakawa <og@hotmail.co.jp>
2018-10-26 00:35:38 +0900

    update mailer

--- src/mailer.js
+++ src/mailer.js
@@ -1,14 +1,24 @@
 const inbox = require('inbox');
 const { simpleParser } = require('mailparser');
 
+const isSendingFromYouTube = ({ from, subject }) => {
+  if (from !== 'YouTube <noreply@youtube.com>') {
+    return false;
+  }
+  if (!subject.includes('just uploaded a video')) {
+    return false;
+  }
+  return true;
+};
+
 const config = {
   port: 993,
   host: 'imap.gmail.com',
   options: {
     secureConnection: true,
     auth: {
-      user: '',
-      pass: '',
+      user: 'hirakawaryouga@gmail.com',
+      pass: '1996129aquarius',
     },
   },
 };
@@ -30,10 +40,23 @@ client.on('new', async (message) => {
   const stream = client.createMessageStream(message.UID);
   const mail = await simpleParser(stream);
 
-  console.log(mail.from);
-  console.log(mail.to);
-  console.log(mail.subject);
-  console.log(mail.text);
+  console.log('From: ', mail.from.text);
+  console.log('To: ', mail.to.text);
+  console.log('Subject: ', mail.subject);
+  console.log('Body: ', mail.html);
+
+  const isFromYouTube = isSendingFromYouTube({
+    from: mail.from.text,
+    subject: mail.subject,
+  });
+
+  if (isFromYouTube) {
+    console.log('This mail is form YouTube.');
+    const re = /(http:\/\/www\.youtube\.com\/watch\?v=.*)\&/gm;
+    const result = re.exec(mail.html)[1].replace('3D', '');
+
+    console.log('URL: ', result);
+  }
 });
 
 client.connect();
