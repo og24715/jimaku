3a72fb387e459eea52bbcf92669f05be582aa90f
hirakawa <og@hotmail.co.jp>
2018-10-26 00:36:25 +0900

    fix assParser

--- src/util/assParser.js
+++ src/util/assParser.js
@@ -3,6 +3,8 @@ const assParser = require('ass-parser');
 module.exports = (ass) => {
   const [scriptInfo, style, events] = assParser(ass);
 
+  events.body.shift();
+
   const filter = events => events.body.map(({ value }) => ({
     start: value.Start,
     end: value.End,
