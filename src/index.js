a3e22a08f08a9417bd070035f9dbb77c0917c982
Unknown <c17009@sangi.jp>
2018-10-25 11:11:37 +0900

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -3,53 +3,87 @@ const assParser = require("./util/assParser");
 const exec = require("child_process").exec;
 //let execSync = require("child_process").execSync;
 const ProgressBar = require("progress");
+const deleteFiles = [ "download.mp4", "download.ja.ass", "burnedsubtitle.mp4" ];
 
 const url = process.argv[2];
 
 const workURL = `youtube-dl --write-auto-sub --sub-lang ja --convert-subs ass -f "mp4" -o download.mp4 ${url}`;
-const workJimaku = `ffmpeg -i download.mp4 -vf ass=download.ja.ass burnedsubtitle.mp4`;
+const workJimaku = 'ffmpeg -i download.mp4 -vf ass=download.ja.ass burnedsubtitle.mp4';
+
+console.log("0/3 start process.");
 
 exec(
   workURL,
-  function(error) {
-    if (error != null) {
+  function(error, stdout, stderr) {
+    if (error || stderr) {
       console.log("Error:" + error);
+      console.log("StdErr:" + stderr);
+      DeleteFile();
       process.exit(1);
     }
-    console.log("1/3 download complete.")
+    console.log("1/3 download complete.");
+
     exec(
       workJimaku,
-      function(error) {
-        if (error != null) {
+      function(error, stdout, stderr) {
+        if (error) {
           console.log("Error:" + error);
+          DeleteFile();
           process.exit(1);
         }
-        console.log("2/3 burn subtitle complete.")
+        console.log("2/3 burn subtitle complete.");
+
         try {
           const readAss = fs.readFileSync("download.ja.ass", "utf-8");
           const subTitles = assParser(readAss);
           let workString;
-          var bar = new ProgressBar(":bar", subTitles.length - 1);
-          subTitles.forEach( function(obj, index) {
-            if (index == 0) return;
-            workString = `ffmpeg -i ../../x2.mp4 -ss ${obj.start} -vframes 1 ../thumbnails/${index}.png`;
-        //   execSync(workString);
-            exec(
-              workString,
-              function(error, stdout, stderr) {
-                if (error != null) {
-                  console.log("Error:" + error);
+          var bar = new ProgressBar("[:bar] :current/:total :percent ", subTitles.length - 1);
+          let countOutputImage = 0;
+          subTitles.forEach(
+            function(obj, index) {
+              if (index == 0) return;
+              workString = `ffmpeg -i burnedsubtitle.mp4 -ss ${obj.start} -vframes 1 thumbnails/${index}.png`;
+              //   同期処理の場合は execSync(workString);
+              exec(
+                workString,
+                function(error, stdout, stderr) {
+                  if (error) {
+                    console.log("Error:" + error);
+                    console.log("StdErr:" + stderr);
+                    DeleteFile();
+                    process.exit(1);
+                  }
+                  bar.tick();
+                  countOutputImage += 1;
+                  // console.log(countOutputImage, subTitles.length);
+                  if (countOutputImage >= subTitles.length - 1) {
+                    console.log("3/3 all complete.")
+                    DeleteFile();
+                  }
                 }
-                bar.tick();
-              }
-            );
-          });
+              );
+            }
+          );
         } catch(e) {
           console.log(e);
+          DeleteFile();
           process.exit(1);
         }
-        console.log("3/3 all complete.")
       }
     );
   }
 );
+
+function DeleteFile() {
+  for (var i = 0; i < deleteFiles.length; i ++) {
+    fs.unlink(
+      deleteFiles[i],
+      (error, stdout, stderr) => {
+        if (error || stderr) {
+          console.log("Error:" + error);
+          console.log("StdErr:" + stderr);
+        }
+      }
+    );
+  }
+}
